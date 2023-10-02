75c142b8cd301a445e88034d5eeb622787361f42
Erwin Antepuesto <matcha.ccy@gmail.com>
2023-10-02 11:39:17 +0800

    [Erwin][Feat]Finalising forms page content

--- src/components/layouts/formsAct.jsx
+++ src/components/layouts/formsAct.jsx
@@ -6,7 +6,24 @@ export default function FormsAct(){
   return (
   <div className="forms-container">
     <div className="left-col">
-      <h1>i am the left col</h1>
+      <h2>Submitted info:</h2>
+      <div className="show-name">
+        <div className="image"></div>
+        <h3>Your name is</h3>
+        <div className="fname"></div>
+        <div className="lname"></div>
+        <h3>You described yourself as: </h3>
+        <div className="about"></div>
+        <h3>More details include: </h3>
+        <div className="gender"></div>
+        <div className="civilStatus"></div>
+        <h3>You are</h3>
+        <div className="age"></div>
+        <div className="birthDate"></div>
+        <h3>Contact info:</h3>
+        <div className="email"></div>
+        <div className="phoneNumber"></div> 
+      </div>
     </div>
     <div className="right-col">
       <FormsBox/>
